90938c1ea32d7a50c90a1a313f98789a3b5becbe
w2cluster244 <w2cluster244@w2cluster293.iiht.tech>
2020-06-07 17:55:27 +0530

    commit

---
+++ StringMessage/src/main/java/com/message/demo/StringMessageApplication.java
@@ -0,0 +1,13 @@
+package com.message.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringMessageApplication.class, args);
+	}
+
+}
